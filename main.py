8ab4774b9e44e9ba52327562c5d1a79afd9dd893
masmir124 <93227535+masmir124@users.noreply.github.com>
2021-12-07 16:34:54 -0600

    Fixed the comments in main.py
    The comments were bad, so we fixed it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Hello World
 print("Hello World")
 # This prints hello world to the terminal.
-''' A computer terminal is an electronic or electromechanical hardware device that can be used for entering data into and transcribing data from a computer or a computing system.
-The teletype was an example of an early day hardcopy terminal, and predated the use of a computer screen by decades. '''
+# A computer terminal is an electronic or electromechanical hardware device that can be used for entering data into and transcribing data from a computer or a computing system.
+# The teletype was an example of an early day hardcopy terminal, and predated the use of a computer screen by decades.
